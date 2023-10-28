269240eb12bc8d90a6425da335c59bb7d0ceddf4
Daddyjohn63 <john@webandprosper.co.uk>
2023-10-28 10:33:08 +0100

    prior to folder rename

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,12 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+//import { displayValue } from '@tanstack/react-query-devtools/build/lib/utils';
 //by using Outlet, all elements around it will be published in all pages
 const HomeLayout = () => {
+  const navigation = useNavigation();
+  //console.log(navigation);
+  const isPageLoading = navigation.state === 'loading';
   return (
     <>
       <Navbar />
       <section className="page">
-        <Outlet />
+        {isPageLoading ? <div className="loading" /> : <Outlet />}
       </section>
     </>
   );
