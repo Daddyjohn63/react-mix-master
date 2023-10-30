f4af205ffbf0d5d986c0ee82d3144d0388c98c0f
Daddyjohn63 <john@webandprosper.co.uk>
2023-10-30 17:16:44 +0000

    setup of react query

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,17 @@
 import axios from 'axios';
 import { useLoaderData } from 'react-router-dom';
 import CocktailList from '../components/CocktailList';
+import SearchForm from '../components/SearchForm';
 
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
-export const loader = async () => {
-  const searchTerm = '';
+export const loader = async ({ request }) => {
+  const url = new URL(request.url); //the request object has a url property.
+  //console.log('request:', request);
+  console.log(url);
+
+  const searchTerm = url.searchParams.get('search') || '';
   const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
   return { drinks: response.data.drinks, searchTerm };
 };
@@ -16,6 +21,7 @@ const Landing = () => {
   // console.log(drinks);
   return (
     <>
+      <SearchForm searchTerm={searchTerm} />
       <CocktailList drinks={drinks} />
     </>
   );
