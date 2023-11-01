e4f1bdd8ce88a39178a528945f7c3970cb8ec8ed
Daddyjohn63 <john@webandprosper.co.uk>
2023-11-01 08:18:10 +0000

    final final

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import Wrapper from '../assets/wrappers/AboutPage';
 const About = () => {
   return (
     <Wrapper>
-      <h3>About 1</h3>
+      <h3>About</h3>
       <p>
         Introducing "MixMaster," the ultimate party sidekick app that fetches
         cocktails from the hilarious Cocktails DB API. With a flick of your
@@ -11,6 +11,15 @@ const About = () => {
         that'll make your taste buds dance and your friends jump with joy. Get
         ready to shake up your mixology game, one fantastical mocktail at a
         time, and let the laughter and giggles flow!
+      </p>
+      <p>
+        Created with React, React Router DOM, React Query, Axios, Styled
+        Components and Toastify. The Newsletter posts to an API which just logs
+        the request, no newsletter is actually sent.
+      </p>
+      <p>
+        The API used to get the cocktails data comes from
+        https://www.thecocktaildb.com/
       </p>
     </Wrapper>
   );
